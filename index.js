238066fce925cbf2f17b5cc6708fbd22ae595a76
Rayhan0Islam0Shagor <67514865+Rayhan0Islam0Shagor@users.noreply.github.com>
2020-09-26 16:42:08 +0600

    node primary concept

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const user = ['asad', 'moin', 'susmita', 'sohana']
 
 // const rootCall = (req, res) => res.send('Thank you very much')
 
 app.get('/', (req, res) => {
-    res.send('I know how to open Node... YAY!');
+    const fruit = {
+        product: 'ada',
+        price: '220'
+    }
+    res.send(fruit);
 })
+
+// app.get('/fruits/banana', (req, res) => {
+//     res.send({
+//         fruit: 'banana',
+//         quantity: 1000,
+//         price: 10000
+//     })
+// })
+
+
+// get
+app.get('/user/:id', (req, res) => {
+    const id = req.params.id;
+    const name = user[id];
+    res.send({ id, name });
+})
+
+// Post
+app.post('/addUser', (req, res) => {
+    // save to database
+    const user = req.body;
+    user.id = 55;
+    res.send(user)
+})
+
 
 app.listen(4000, () => console.log('listening to port 4000'))
